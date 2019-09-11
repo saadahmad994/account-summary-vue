b22c0f5f6297c2f7629c0ea8905ecc0b0cea642e
Saad Ahmad <saad.ahmad994@gmail.com>
2019-09-11 16:04:21 +0530

    test
    demo

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,33 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" />
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank">Core Docshjhjhjhjh</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank">Forum</a>
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank">Community Chat</a>
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
-      </li>
-      <li>
-        <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
-      </li>
-      <li>
-        <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
-      </li>
-      <li>
-        <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
-      </li>
-    </ul>
+    <test />
   </div>
 </template>
 
 <script>
+import test from "./test";
 export default {
   name: "app",
+  components: {
+    test: test
+  },
   data() {
     return {
-      msg: "Account Summary Page Setup"
+      title: "My First Vue.js App",
+      friends: [
+        { name: "Saad", online: true },
+        { name: "Deepak", online: true },
+        { name: "Puneet", online: false },
+        { name: "Abhishek", online: false }
+      ]
     };
+  },
+  methods: {
+    deleteFriend(payload) {
+      this.friends = this.friends.filter(friend => {
+        return friend.name !== payload.name;
+      });
+    }
   }
 };
 </script>
